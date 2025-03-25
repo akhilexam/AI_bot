53017b2b7af9fa494fb316f8b98d85da87f8b00e
akhilexam <125046861+akhilexam@users.noreply.github.com>
2025-03-25 16:46:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ SUPABASE_URL = os.getenv("SUPABASE_URL", "https://khvqmaiybkwzuzsdjnwm.supabase.
 SUPABASE_KEY = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtodnFtYWl5Ymt3enV6c2RqbndtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4ODY0NDEsImV4cCI6MjA1ODQ2MjQ0MX0.tEDa9X-d74adQ7K0yyFhreP1Ar4ziolUj6wfWPZwhdM")
 
 # OpenAI API Key
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
